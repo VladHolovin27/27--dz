3619b1f0573749dd6a02706f89e30f7aa58c4857
VladHolovin27 <105449427+VladHolovin27@users.noreply.github.com>
2023-08-25 18:10:31 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+//#1//
+//  let t = prompt();
+//  let obj = {
+//      temperature: 27,
+//      humidity: 50,
+//      windSpeed: 59,
+//      'calculateTemperature': function() {
+//          if(temperature < 0) {
+//              console.log(true);
+//              console.log("Так!");
+//          }
+//          else if(temperature >= 0) {
+//             console.log(false);
+//              console.log("Ні!");
+//          }
+//      }
+//  }
+
+
+//#2//
+// let name = prompt();
+// let email = prompt();
+// let password = prompt();
+
+// let obj = {
+//   name: 'name',
+//   password: 'password',
+//   email: 'email',
+//   login() {
+//     if(email.includes('@', '.com') && password >= 8) {
+//         alert("Все Правильно! :");
+//     } else {
+//         alert('Неправильно! :');
+//     }  
+//   }
+// }
+// obj.login();
+
+//#3//
+// let a = prompt();
+// let movie = {
+//     title: 'Vlad is 14',
+//     director: 50,
+//     year: 2017,
+//     rating: 4,
+//     'calculateRating': function() {
+//         if(rating > 8) {
+//             console.log(true);
+//             title.style.color = 'green';
+//         }
+//         else if(rating <= 8) {
+//             console.log(false);
+//               title.style.color = 'red';
+//         }
+//     }
+// }
